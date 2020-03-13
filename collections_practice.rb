31bf2e4938c936646f6f42d29d084477826286ce
Ty Raino <mstymetris@yahoo.com>
2020-03-13 05:47:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,3 +19,9 @@ end
 def swap_elements(array)
 array[0], array[1], array[2] = array[0], array[2], array[1]
 end
+
+def reverse_array(numbers)
+  numbers.collect do |num|
+    num.reverse
+  end
+end
